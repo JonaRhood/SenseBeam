4ab60a7e9854ae176ee3bada1206837f95b7003d
Jonathan Cano <jonathancanofreta@gmail.com>
2025-04-26 19:25:55 +0200

    Updating server port to fix azure instructions

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const next = require('next');
 const http = require('http');
 const WebSocket = require('ws');
 
-const port = 8080;
+const port = parseInt(process.env.PORT, 10) || 8080;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
