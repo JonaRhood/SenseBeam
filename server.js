bbb28dd31779fdef6eae24b0d1f3b2546c1c487d
Jonathan Cano <jonathancanofreta@gmail.com>
2025-04-26 19:17:24 +0200

    Replaced port number to fit azure conf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const next = require('next');
 const http = require('http');
 const WebSocket = require('ws');
 
-const port = process.env.PORT;
+const port = 8080;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
