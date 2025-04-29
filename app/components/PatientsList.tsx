966ee9d369019049c338ee842d87a9174e622abd
Jonathan Cano <jonathancanofreta@gmail.com>
2025-04-29 17:43:31 +0200

    Patients List Added using Mock API

--- app/components/PatientsList.tsx
+++ app/components/PatientsList.tsx
@@ -1,47 +1,105 @@
 // app/components/PatientsList.tsx
 import { useEffect, useState } from "react"
+import Image from "next/image"
+
+interface PatientsListProps {
+    searchText: string;
+}
 
 interface Patient {
     id: number,
+    image: string,
     firstName: string,
     lastName: string,
+    age: number,
+    gender: string,
+    bloodGroup: string;
     email: string,
-    age: number
-  }
+}
 
-export default function PatientsList() {
+export default function PatientsList({ searchText }: PatientsListProps) {
 
+    const [fullList, setFullList] = useState<Patient[]>([]);
     const [list, setList] = useState<Patient[]>([]);
-
+    
     useEffect(() => {
         const fetchPatients = async () => {
             const dataPatients = await fetch('https://dummyjson.com/users');
             const result = await dataPatients.json();
 
+            result.users.sort((a: Patient, b: Patient) => a.lastName.localeCompare(b.lastName));
+
+            console.log(result.users)
             setList(result.users);
+            setFullList(result.users);
         }
 
         fetchPatients();
     }, []);
 
+    useEffect(() => {
+        const inputSearch = fullList.filter((patient) => {
+            const fullName = `${patient.firstName} ${patient.lastName}`.toLowerCase();
+            return fullName.includes(searchText.toLowerCase());
+        });
+        setList(inputSearch);
+    }, [searchText, fullList]);
+
+
+    const [filter, setFilter] = useState<string>("ASC");
+
+    const filterByType = (type: keyof Patient) => {
+        const sortedList = [...list].sort((a, b) => {
+            const aValue = a[type];
+            const bValue = b[type];
+
+            if (typeof aValue === "number" && typeof bValue === "number") {
+                return filter === "ASC" ? aValue - bValue : bValue - aValue;
+            }
+
+            return filter === "ASC"
+                ? String(aValue).localeCompare(String(bValue))
+                : String(bValue).localeCompare(String(aValue));
+        });
+
+        setList(sortedList);
+        setFilter(filter === "ASC" ? "DESC" : "ASC");
+    };
+
+
     return (
-        <div className="flex w-full h-full border-2 border-green-500">
-            <table className="w-full border border-gray-300 shadow-md rounded-md">
-                <thead className="bg-gray-100">
+<div className="flex w-full max-h-[80vh]">
+
+            <table className="w-full shadow-md rounded-md">
+                <thead className="bg-blue-100 sticky top-0 z-10 w-full">
                     <tr>
-                        <th className="text-left p-2 border-b">Nombre</th>
-                        <th className="text-left p-2 border-b">Apellido</th>
-                        <th className="text-left p-2 border-b">Email</th>
-                        <th className="text-left p-2 border-b">Edad</th>
+                        <th className="thPatientsList">Imagen</th>
+                        <th className="thPatientsList" onClick={() => filterByType("lastName")}>Apellidos</th>
+                        <th className="thPatientsList" onClick={() => filterByType("firstName")}>Nombre</th>
+                        <th className="thPatientsList" onClick={() => filterByType("age")}>Edad</th>
+                        <th className="thPatientsList" onClick={() => filterByType("gender")}>Gender</th>
+                        <th className="thPatientsList" onClick={() => filterByType("bloodGroup")}>ABO</th>
+                        <th className="thPatientsList">Email</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="">
                     {list.map((patient) => (
-                        <tr key={patient.id} className="hover:bg-gray-50">
-                            <td className="p-2 border-b">{patient.firstName}</td>
-                            <td className="p-2 border-b">{patient.lastName}</td>
-                            <td className="p-2 border-b">{patient.email}</td>
-                            <td className="p-2 border-b">{patient.age}</td>
+                        <tr key={patient.id} className="hover:bg-blue-50 hover:cursor-pointer">
+                            <td className="justify-center flex p-2 border-y-[0.5px] border-blue-200">
+                                <Image
+                                    src={`${patient.image}`}
+                                    width={80}
+                                    height={80}
+                                    alt={`Imagen de ${patient.firstName} ${patient.lastName} `}
+                                    className="rounded-full border-[1px] border-blue-300"
+                                />
+                            </td>
+                            <td className="tdPatientsList">{patient.lastName}</td>
+                            <td className="tdPatientsList">{patient.firstName}</td>
+                            <td className="tdPatientsList">{patient.age}</td>
+                            <td className="tdPatientsList">{patient.gender}</td>
+                            <td className="tdPatientsList">{patient.bloodGroup}</td>
+                            <td className="tdPatientsList">{patient.email}</td>
                         </tr>
                     ))}
                 </tbody>
--- app/components/SearchPatients.tsx
+++ app/components/SearchPatients.tsx
@@ -1,16 +1,30 @@
 // app/components/SearchPatients.tsx
 
+import { useState } from "react";
 import PatientsList from "./PatientsList"
 
 export default function SearchPatients() {
+    const [ searchText, setSearchText ] = useState("");
+
+    const inputSearch = (e : any) => {
+        if (e.key == "Enter") {
+            e.preventDefault();
+            setSearchText(e.target.value);
+        } else if (e.key == "Backspace" && e.target.value.length <= 1) {
+            setSearchText(e.target.value);
+        }
+    }
 
     return (
-    <div className="flex flex-col h-full border-2 border-red-500">
-      <div className="flex justify-center h-[8svh] items-center border-2 border-green-500 ">
-        Hola
+    <div className="flex flex-col h-full">
+      <div className="flex justify-center h-[8svh] items-center">
+        <input 
+            className="border-[1px] border-blue-300 rounded-full w-[45%] px-4 p-1"
+            onKeyDown={(e) => inputSearch(e)}
+        ></input>
       </div>
-      <div className="flex justify-center h-full items-center border-2 border-blue-500 overflow-hidden overflow-y-auto">
-        <PatientsList />
+      <div className="flex justify-center overflow-hidden overflow-y-scroll overflow-x-auto">
+        <PatientsList searchText={searchText} />
       </div>
     </div>
     )
